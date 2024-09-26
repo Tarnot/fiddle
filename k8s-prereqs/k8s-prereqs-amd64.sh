0d240a12faf53b1643251acbb7aa502f9d0dd3e0
Studio Arkhein <studioarkhein@gmail.com>
2024-09-26 16:19:16 -0500

    sda

--- k8s-prereqs/k8s-prereqs-amd64.sh
+++ k8s-prereqs/k8s-prereqs-amd64.sh
@@ -128,3 +128,5 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
+
+echo "ON CONTROL PLANE: kubeadm token create --print-join-command"
