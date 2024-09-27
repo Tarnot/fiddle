c874e768d92acf4be71692207426fef7de5c068c
Studio Arkhein <studioarkhein@gmail.com>
2024-09-27 16:07:59 -0500

    Update for end

--- k8s-prereqs/k8s-prereqs-arm64-controlplane.sh
+++ k8s-prereqs/k8s-prereqs-arm64-controlplane.sh
@@ -40,39 +40,22 @@ sudo chown fiddle:fiddle $MYHOME/install-k8s/custom-resources.yaml;
 sudo find $MYHOME/install-k8s/custom-resources.yaml -type f -exec sed -i 's/\/16/\/23/g' {} \;
 sudo -H -u fiddle bash -c 'kubectl create -f custom-resources.yaml'
 
-echo "          ##################################################";
-
-# Check Installation - https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+# Print Instructions to Screen
+echo "CHECKING CLUSTER:"
 sudo -H -u fiddle bash -c 'kubectl cluster-info'
-
-echo "          watch kubectl get pods -n calico-system;";
-echo "          sudo kubectl get pods --all-namespaces;";
-
-echo "          ##################################################";
-# echo instructions
+echo "CHECK CALICO:"
+echo "watch kubectl get pods -n calico-system;";
+echo "sudo kubectl get pods --all-namespaces;";
 echo "ON CONTROL PLANE RUN:";
-echo "          sudo echo 'source <(kubectl completion bash)' >> $MYHOME/.bashrc";
-echo "          sudo echo 'alias k=kubectl' >> $MYHOME/.bashrc";
-echo "          sudo echo 'complete -o default -F __start_kubectl k' >> $MYHOME/.bashrc";
-echo "          source $MYHOME/.bashrc";
-
-echo "          ##################################################";
-#echo "          sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml;";
-
-echo "          sudo kubectl taint nodes --all node-role.kubernetes.io/control-plane-;";
-
-
-echo "          sudo kubeadm token create --print-join-command;";
-echo "COPY RESULTING TEXT";
-echo "RUN ON EACH NODE (SOMETHING LIKE):";
-echo "          kubeadm join 192.168.254.210:6443 --token 6yglpi.f3tcar2s4qybckr7 --discovery-token-ca-cert-hash sha256:ad71aac8aff7ee25cf0eb5725a54c4990d0016849ace302d9235b3adcab57374";
+echo "sudo echo 'source <(kubectl completion bash)' >> $MYHOME/.bashrc";
+echo "sudo echo 'alias k=kubectl' >> $MYHOME/.bashrc";
+echo "sudo echo 'complete -o default -F __start_kubectl k' >> $MYHOME/.bashrc";
+echo "source $MYHOME/.bashrc";
+echo "RUN ON EACH NODE:";
+sudo kubeadm token create --print-join-command;
 echo "TO ACCESS kubectl FROM ANOTHER COMPUTER:";
-echo "          scp root@<control-plane-host>:/etc/kubernetes/admin.conf .;";
-echo "          kubectl --kubeconfig ./admin.conf get nodes;";
-
-
-
-
+echo "scp root@<control-plane-host>:/etc/kubernetes/admin.conf .;";
+echo "kubectl --kubeconfig ./admin.conf get nodes;";
 
 # End of Script
 ##################################################
